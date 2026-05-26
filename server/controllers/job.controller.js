2f60d17cd8621fb386650a2993ec053e47c4f0da
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-05-26 09:52:41 -0700

    thumbnail fixed

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -2,6 +2,11 @@ import { startJob, getJob } from "../services/job.service.js";
 import fs from "fs/promises";
 import path from "path";
 import ffmpeg from "fluent-ffmpeg";
+import ffmpegPath from "ffmpeg-static";
+import ffprobe from "ffprobe-static";
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobe.path);
 
 export async function startProcessingJob(req, res) {
     try {
